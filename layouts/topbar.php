b089ed7f090837b0b760b74404269041a921c4ee
MaoRusk <manolo_rod.98@hotmail.com>
2025-11-10 17:12:11 -0600

    Change options language topbar

--- layouts/topbar.php
+++ layouts/topbar.php
@@ -773,29 +773,10 @@
                         <h6 class="dropdown-header"><?php echo 'Welcome ' . htmlspecialchars(getCurrentUserName() ?: 'Usuario') . '!'; ?></h6>
                         <a class="dropdown-item" href="pages-profile-settings.php"><i
                                 class="mdi mdi-account-circle text-muted fs-16 align-middle me-1"></i> <span
-                                class="align-middle">Profile</span></a>
-                        <!-- <a class="dropdown-item" href="apps-chat.php"><i
-                                class="mdi mdi-message-text-outline text-muted fs-16 align-middle me-1"></i> <span
-                                class="align-middle">Messages</span></a>
-                        <a class="dropdown-item" href="apps-tasks-kanban.php"><i
-                                class="mdi mdi-calendar-check-outline text-muted fs-16 align-middle me-1"></i> <span
-                                class="align-middle">Taskboard</span></a>
-                        <a class="dropdown-item" href="pages-faqs.php"><i
-                                class="mdi mdi-lifebuoy text-muted fs-16 align-middle me-1"></i> <span
-                                class="align-middle">Help</span></a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="pages-profile.php"><i
-                                class="mdi mdi-wallet text-muted fs-16 align-middle me-1"></i> <span
-                                class="align-middle">Balance : <b>$5971.67</b></span></a>
-                        <a class="dropdown-item" href="pages-profile-settings.php"><span
-                                class="badge bg-soft-success text-success mt-1 float-end">New</span><i
-                                class="mdi mdi-cog-outline text-muted fs-16 align-middle me-1"></i> <span
-                                class="align-middle">Settings</span></a>
-                        <a class="dropdown-item" href="auth-lockscreen-basic.php"><i
-                                class="mdi mdi-lock text-muted fs-16 align-middle me-1"></i> <span class="align-middle">Lock screen</span></a> -->
+                                class="align-middle">Perfil</span></a>
                         <a class="dropdown-item" href="logout.php"><i
                                 class="mdi mdi-logout text-muted fs-16 align-middle me-1"></i> <span
-                                class="align-middle" data-key=t-logout><?=$lang['t-logout']?></span></a>
+                                class="align-middle" data-key=t-logout>Cerrar sesión</span></a>
                     </div>
                 </div>
             </div>
